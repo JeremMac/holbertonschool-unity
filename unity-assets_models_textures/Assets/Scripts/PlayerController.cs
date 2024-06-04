03c68de70e3eeb321b8ef7557d97cc023bbb7708
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-06-04 17:46:22 +0200

    Added a movement script to the player an repositioned camera.

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public Rigidbody player;
+	public float speed ;
+	public float jumpForce = 10f;
+    public bool isGrounded = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Récupération des entrées du joueur
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+		transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+		transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			player.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+			isGrounded = false;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			isGrounded = true;
+		}
+	}
+}
