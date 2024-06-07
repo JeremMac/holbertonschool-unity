08051b695a2cfcfcee6b9a606973206c1bdc5d0a
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-06-07 11:00:46 +0200

    Added a skyboxe to the camera.

---
+++ unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public Text TimerText;
+	public float currentTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void StartTimer()
+	{
+		currentTime += Time.deltaTime;
+		int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int centiseconds = Mathf.FloorToInt((currentTime * 100) % 100);
+        TimerText.text = string.Format("{0:0}.{1:00}.{2:00}", minutes, seconds, centiseconds);
+	}
+}
---
+++ unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour {
+	public TimerTrigger timeStop;
+	public Text winText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			timeStop.timerRolling = false;
+			winText.color = Color.green;
+			winText.fontSize = 60;
+		}
+	}
+}
