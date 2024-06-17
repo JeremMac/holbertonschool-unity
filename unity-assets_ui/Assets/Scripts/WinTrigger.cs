60c7311a822689ce05d7aaea0395aa593723c01a
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-06-17 15:02:06 +0200

    Created UI directory and completed task 0.

---
+++ unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour {
+	public TimerTrigger timeStop;
+	public TextMeshProUGUI winText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			timeStop.timerRolling = false;
+			winText.color = Color.green;
+			winText.fontSize = 60;
+		}
+	}
+}
---
+++ unity-assets_ui/README.md
@@ -0,0 +1,9 @@
+# Unity assets models and textures directory.
+
+<p>
+<p>Credits:</p>
+<p>Skyboxes: Farland Skies - Cloudy Crown</p>
+<p>Link: https://assetstore.unity.com/packages/2d/textures-materials/sky/farland-skies-cloudy-crown-60004</p>
+<p>Models: Kenney's Nature Pack Extended</p>
+<p>Link: https://kenney.nl/assets/nature-kit</p>
+</p>
