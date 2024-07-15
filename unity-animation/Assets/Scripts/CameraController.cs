84fd63149c1570b4d34ec8898c3e7e76a6e686ea
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-07-15 11:24:22 +0200

    Added the unity animation project folder.

---
+++ unity-animation/Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameSettings
+{
+    public static bool isInverted = false;
+}
+public class CameraController : MonoBehaviour {
+
+	public float mouseSensitivity;
+	public float smoothTime;
+	private float _rotationY;
+	private float _rotationX;
+	public Transform target;
+	public float distanceFromTarget;
+	private Vector3 _currentLocation;
+	private Vector3 _smoothVelocity = Vector3.zero;
+	public bool isInverted;
+	public static CameraController instance;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		instance = this;
+        isInverted = GameSettings.isInverted;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+		if (isInverted == false)
+        {
+            _rotationY += mouseX;
+            _rotationX -= mouseY;
+
+        }
+        else
+        {
+            _rotationY += mouseX;
+            _rotationX += mouseY;
+        }
+
+		_rotationX = Mathf.Clamp(_rotationX, -30, 30);
+
+		Vector3 nextRotation = new Vector3(_rotationX, _rotationY);
+
+		//Vector3 playerRotation = new Vector3(0, _rotationY);
+		_currentLocation = Vector3.SmoothDamp(_currentLocation, nextRotation, ref _smoothVelocity, smoothTime);
+
+		transform.localEulerAngles = _currentLocation;
+
+		transform.position = target.position - transform.forward * distanceFromTarget;
+		target.rotation = transform.rotation;
+		//target.rotation = Quaternion.Euler(transform.rotation.x, 0f, 0f);
+	}
+}
---
+++ unity-animation/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Toggle isInverted;
+
+
+    public void Back()
+    {
+        SceneController.instance.LoadPreviousScene();
+    }
+    public void Apply()
+    {
+        if (isInverted.isOn)
+        {
+            GameSettings.isInverted = true;
+            SceneController.instance.LoadPreviousScene();
+        }
+        else
+        {
+            GameSettings.isInverted = false;
+            SceneController.instance.LoadPreviousScene();
+        }
+    }
+    
+}
